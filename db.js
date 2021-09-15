80e00d4511d24d9a9c8802509414391f9b7775b3
Slobodan Bajić <78023645+slobodanbajic9@users.noreply.github.com>
2021-09-15 14:01:52 +0200

    Add files via upload

---
+++ db.js
@@ -0,0 +1,23 @@
+let db = [
+  {
+    id: 1,
+    name: 'Slobodan',
+    lastname: 'Bajic',
+    email: 'slobodan@gmail.com',
+    phone: '11-11-11-11'
+  },
+  {
+    id: 2,
+    name: 'Mirko',
+    lastname: 'Mirkovic',
+    email: 'mirko@gmail.com',
+    phone: '22-22-22-22'
+  },
+  {
+    id: 3,
+    name: 'Marija',
+    lastname: 'Andjelkovic',
+    email: 'marija@gmail.com',
+    phone: '33-33-33-33'
+  }
+];
